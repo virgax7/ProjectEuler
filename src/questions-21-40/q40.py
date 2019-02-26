0c2f653befd0f5b0275a0581c529d0931c84ad67
mlnoob <pyoungx7@gmail.com>
2019-02-25 21:19:48 -0500

    q40

---
+++ src/questions-21-40/q40.py
@@ -0,0 +1,12 @@
+# An irrational decimal fraction is created by concatenating the positive integers:
+#
+# 0.123456789101112131415161718192021...
+#
+# It can be seen that the 12th digit of the fractional part is 1.
+#
+# If dn represents the nth digit of the fractional part, find the value of the following expression.
+#
+# d1 × d10 × d100 × d1000 × d10000 × d100000 × d1000000
+
+s = '.' + ''.join(str(i) for i in range(1, 1000000))
+print(int(s[1]) * int(s[10]) * int(s[100]) * int(s[1000]) * int(s[10000]) * int(s[100000]) * int(s[1000000]))
