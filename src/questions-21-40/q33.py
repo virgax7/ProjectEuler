d8d049d6a233c7178f364c1d0106591c63b08942
Mike Baik <baik.mike@gmail.com>
2019-03-02 13:50:31 -0500

    Question 33

--- src/questions-21-40/q33.py
+++ src/questions-21-40/q33.py
@@ -7,5 +7,29 @@
 # less than one in value, and containing two digits in the numerator and denominator.
 #
 # If the product of these four fractions is given in its lowest common terms, find the value of the denominator.
+import functools
+import math
+from operator import mul
 
-# not sure what it wants so skipping this one 
+
+def get_curious_fractions():
+    temps = [(num, denum)
+            for num in range(1, 99)
+                for denum in range(num+1, 100)
+                    if (num % 10 == denum // 10
+                        and denum % 10 != 0
+                        and (num // 10) / (denum % 10) == num / denum)]
+    nums, denums = zip(*temps)
+    return nums, denums
+
+
+nums, denums = get_curious_fractions()
+
+numerators = functools.reduce(mul, nums)
+denomenators = functools.reduce(mul, denums)
+
+print(denomenators/math.gcd(numerators, denomenators))
+
+
+
+
